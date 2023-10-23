bb985f418517ee9ed26f4d7714b156e59f30320c
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-10-23 18:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let p=document.getElementById("status");
+let btn=document.getElementById("enterBtn");
+
+btn.addEventListener("click",f);
+function f(){
+	p.innerHTML="Entered Metaverse";
+}
